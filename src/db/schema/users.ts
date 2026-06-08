ba6bf6b37085fc47605c0cf767fd39c1ba381234
htmujahid <htmujahid@gmail.com>
2026-06-08 08:04:16 +0500

    feat(auth): implement new authentication schema and routes

--- src/db/schema/users.ts
+++
@@ -1,9 +0,0 @@
-import { integer, pgTable, varchar, timestamp } from 'drizzle-orm/pg-core'
-
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-})
