a728bf14f608d813c57c02f38e6f17dae9f66059
htmujahid <htmujahid@gmail.com>
2026-06-08 10:46:37 +0500

    feat(auth): implement email functionality for password reset and verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
 import { db } from '@/db'
+import { mailer } from '@/lib/mailer'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -9,6 +10,25 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      void mailer.sendMail({
+        from: 'noreply@tarteebai.com',
+        to: user.email,
+        subject: 'Reset your password',
+        text: `Click the link to reset your password: ${url}`,
+      })
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      void mailer.sendMail({
+        from: 'noreply@tarteebai.com',
+        to: user.email,
+        subject: 'Verify your email address',
+        text: `Click the link to verify your email: ${url}`,
+      })
+    },
   },
   socialProviders: {
     google: {
